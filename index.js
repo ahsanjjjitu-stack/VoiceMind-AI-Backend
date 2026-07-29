0781a98608244e0d19410f0326679394e5d4606e
JJ Jitu Ahsan <ahsanjjjitu@gmail.com>
2026-07-29 14:06:05 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const authRouters = require('./routes/auth');
+const authRouters = require('./router/auth');
 
 // set dns servers to avoid DNS resolution issues
 const dns = require('dns');
