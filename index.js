7647da826cc7f6751bd0c5d639d67a63ac0ca6bc
JJ Jitu Ahsan <ahsanjjjitu@gmail.com>
2026-07-30 16:40:22 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.log(err));
 
-app.use('/auth', authRouters);
+app.use('/api/auth', authRouters);
 
 
 
